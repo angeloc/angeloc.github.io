c80170efe6f9be5c195463ad045ac3970e316079
Angelo Compagnucci <angelo.compagnucci@gmail.com>
2015-10-06 00:05:08 +0200

    Further improvements

--- js/buildroot.js
+++ js/buildroot.js
@@ -53,6 +53,21 @@ function google_analytics() {
     s.parentNode.insertBefore(ga, s);
 }
 
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
 google.load("feeds", "1");
 google.setOnLoadCallback(initialize);
 google_analytics();
@@ -63,3 +78,35 @@ jQuery(document).ready(function($) {
     url = url.split(/[\\/]/).pop()
     $('.nav a[href="/' + url + '"]').parent().addClass('active');
 });
+
+function showTooltip(elem, msg) {
+    elem.setAttribute('class', 'btn tooltipped tooltipped-s');
+    elem.setAttribute('aria-label', msg);
+}
+
+var clipboard = new Clipboard('.btn');
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+clipboard.on('success', function(e) {
+    e.clearSelection();
+    $(e.trigger).tooltip('show');
+});
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
